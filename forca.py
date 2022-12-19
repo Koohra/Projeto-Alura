2a35285123218ef9d90ac048ea76ce63fecd86f4
Miguel Pereira de Sousa <Miguel.pereira.12@live.com>
2022-12-19 20:51:16 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,8 +1,29 @@
 def jogar():
-    
     print('*' * 28)
     print('Bem vindo ao jogo da Forca!')
     print('*' * 28)
+    
+    palavra_secreta = 'gato'
+    letras_acertadas = ["_", "_", "_", "_",]
+    
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        
+        chute = str(input('Qual letra?')).strip().lower()
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                letras_acertadas[index] = letra
+                print(f'Encontrei a letra {chute} na posição {index}')
+            index += 1
+
+        print(letras_acertadas)
+
     
     print('Fim do jogo')
 
